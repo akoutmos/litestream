199ffd3b82e3a22c90fc8a774f8fd6a039004825
Alex Koutmos <akoutmos@gmail.com>
2022-03-03 13:36:08 -0500

    Published to placehold Hex name

--- mix.exs
+++ mix.exs
@@ -4,10 +4,16 @@ defmodule Litestream.MixProject do
   def project do
     [
       app: :litestream,
-      version: "0.1.0",
+      version: "0.1.0-beta",
       elixir: "~> 1.13",
+      name: "Litestream",
+      source_url: "https://github.com/akoutmos/litestream",
+      homepage_url: "https://hex.pm/packages/litestream",
+      description: "Add Litestream to your SQLite powered application for effortless backups",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -23,7 +29,30 @@ defmodule Litestream.MixProject do
     [
       {:erlexec, "~> 1.19"},
       {:castore, "~> 0.1.15"},
-      {:ex_doc, "~> 0.28.2", only: :docs}
+      {:ex_doc, "~> 0.28.2", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: "litestream",
+      files: ~w(lib mix.exs README.md),
+      licenses: ["MIT"],
+      maintainers: ["Alex Koutmos"],
+      links: %{
+        "GitHub" => "https://github.com/akoutmos/litestream",
+        "Sponsor" => "https://github.com/sponsors/akoutmos"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "master",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
