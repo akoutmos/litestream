7a20df45b5ac6d8eee9f623ee02622ce4f119346
Alex Koutmos <akoutmos@gmail.com>
2022-03-03 12:41:42 -0500

    PoC working

--- lib/litestream.ex
+++ lib/litestream.ex
@@ -34,7 +34,8 @@ defmodule Litestream do
       repo: Keyword.fetch!(opts, :repo),
       replica_url: Keyword.fetch!(opts, :replica_url),
       access_key_id: Keyword.fetch!(opts, :access_key_id),
-      secret_access_key: Keyword.fetch!(opts, :secret_access_key)
+      secret_access_key: Keyword.fetch!(opts, :secret_access_key),
+      version: Keyword.get(opts, :version, Downloader.latest_version())
     }
 
     GenServer.start_link(__MODULE__, state, name: Keyword.get(opts, :name, __MODULE__))
@@ -72,13 +73,78 @@ defmodule Litestream do
 
     updated_state =
       state
-      |> Map.put(:otp_app)
-      |> Map.put(:database)
+      |> Map.put(:otp_app, otp_app)
+      |> Map.put(:database, database_file)
 
     {:ok, updated_state, {:continue, :download_litestream}}
   end
 
   @impl true
-  def handle_continue(:download_litestream, state) do
+  def handle_continue(:download_litestream, %{otp_app: otp_app, version: version} = state) do
+    otp_app_priv_dir = :code.priv_dir(otp_app)
+    download_dir = Path.join(otp_app_priv_dir, "/litestream/download")
+    bin_dir = Path.join(otp_app_priv_dir, "/litestream/bin")
+
+    File.mkdir_p!(download_dir)
+    File.mkdir_p!(bin_dir)
+
+    {:ok, bin_path} = Downloader.download_litestream(version, download_dir, bin_dir)
+
+    updated_state = Map.put(state, :bin_path, bin_path)
+
+    {:noreply, updated_state, {:continue, :start_litestream}}
+  end
+
+  def handle_continue(:start_litestream, state) do
+    {:ok, port_pid, _os_pid} =
+      :exec.run_link(
+        "#{state.bin_path} replicate #{state.database} #{state.replica_url}",
+        [
+          :monitor,
+          {:env,
+           [
+             :clear,
+             {"LITESTREAM_ACCESS_KEY_ID", state.access_key_id},
+             {"LITESTREAM_SECRET_ACCESS_KEY", state.secret_access_key}
+           ]},
+          {:kill_timeout, 10},
+          :stdout,
+          :stderr
+        ]
+      )
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:EXIT, _os_pid, reason}, state) do
+    Logger.info("Litestream has exited with reason: #{reason}")
+
+    {:noreply, state}
+  end
+
+  def handle_info({:DOWN, _os_pid, _process, _pid, reason}, state) do
+    Logger.info("Litestream has shutdown with reason: #{reason}")
+
+    {:noreply, state}
+  end
+
+  def handle_info({:stdout, _os_pid, output}, state) do
+    Logger.info(output)
+
+    {:noreply, state}
+  end
+
+  def handle_info({:stderr, _os_pid, output}, state) do
+    Logger.warn(output)
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def terminate(reason, _state) do
+    Logger.info("Litestream is shutting down with reason #{inspect(reason)}")
+
+    :ok
   end
 end
